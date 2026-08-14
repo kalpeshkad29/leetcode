609b3578e1e7d5295fe385bba2d419796b9aa2bd
kalpeshkad29 <kalpeshjk1111@gmail.com>
2026-08-14 14:39:45 +0530

    Time: 0 ms (100%), Space: 20.6 MB (35.72%) - LeetHub

--- 0704-binary-search/0704-binary-search.py
+++ 0704-binary-search/0704-binary-search.py
@@ -7,9 +7,9 @@ class Solution:
             mid=(low+high)//2
             if nums[mid]==target:
                 return mid
-            elif nums[mid]>target:
-                high = mid-1
+            if nums[mid]>target:
+                high=mid-1
             else:
                 low=mid+1
         return -1
-        
+       
