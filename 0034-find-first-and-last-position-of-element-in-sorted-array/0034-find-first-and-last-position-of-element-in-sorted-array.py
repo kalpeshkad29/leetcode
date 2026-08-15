5be8efd0d5700ffc84fe24f545684c66f0d2fda8
kalpeshkad29 <kalpeshjk1111@gmail.com>
2026-08-15 09:52:42 +0530

    Time: 0 ms (100%), Space: 20.7 MB (28.01%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.py
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.py
@@ -1,14 +1,39 @@
 class Solution:
-    def searchRange(self, nums: List[int], target: int) -> List[int]:
-        first=-1
-        last=-1
+    def lowerBound(self,nums,target):
         n=len(nums)
-        for i in range(0,n):
-            if nums[i]==target:
-                if first==-1:
-                    first=i
-                
-                last=i
-        return [first,last]
-
+        lb=n
+        low=0
+        high=n-1
+        while low<=high:
+            mid=(low+high)//2
+            
+            if nums[mid]>=target:
+                lb=mid
+                high=mid-1
+            else:
+                low=mid+1
+        return lb
+    
+    def upperBound(self,nums,target):
+        n=len(nums)
+        low=0
+        high=n-1
+        ub=n
+        while low<=high:
+            mid=(low+high)//2
+            if nums[mid]>target:
+                ub=mid
+                high=mid-1
+            else:
+                low=mid+1
+        return ub
+    def searchRange(self, nums: List[int], target: int) -> List[int]:
+        lb=self.lowerBound(nums,target)
+        ub=self.upperBound(nums,target)
+        if lb==len(nums) or nums[lb]!=target:
+            return [-1,-1]
+        return [lb,ub-1]
+    
+        
+        
         
