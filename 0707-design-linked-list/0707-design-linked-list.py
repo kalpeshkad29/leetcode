0f4f23f5e9c15b6643416807d1c7894307b2db75
kalpeshkad29 <kalpeshjk1111@gmail.com>
2026-08-16 10:32:29 +0530

    Time: 86 ms (11.53%), Space: 20.1 MB (76.62%) - LeetHub

---
+++ 0707-design-linked-list/0707-design-linked-list.py
@@ -0,0 +1,88 @@
+class Node:
+    def __init__(self,val):
+        self.val=val
+        self.next=None
+class MyLinkedList:
+
+    def __init__(self):
+        self.head=None
+        
+
+    def get(self, index: int) -> int:
+        curr=self.head
+        while curr and index>0:
+            curr=curr.next
+            index-=1
+        if curr:
+            return curr.val
+        return -1
+        
+
+    def addAtHead(self, val: int) -> None:
+        new_node=Node(val)
+        new_node.next=self.head
+        self.head=new_node
+        
+
+    def addAtTail(self, val: int) -> None:
+        new_node=Node(val)
+        if self.head is None:
+            self.head=new_node
+            return
+        curr=self.head
+        while curr.next:
+            curr=curr.next
+        curr.next=new_node
+        
+
+    def addAtIndex(self, index: int, val: int) -> None:
+
+        if index == 0:
+            self.addAtHead(val)
+            return
+
+        curr = self.head
+        pos = 0
+
+        while curr and pos < index - 1:
+            curr = curr.next
+            pos += 1
+
+        if curr is None:
+            return
+
+        new_node = Node(val)
+        new_node.next = curr.next
+        curr.next = new_node
+
+
+    def deleteAtIndex(self, index: int) -> None:
+
+        if self.head is None:
+            return
+
+        if index == 0:
+            self.head = self.head.next
+            return
+
+        curr = self.head
+        pos = 0
+
+        while curr.next and pos < index - 1:
+            curr = curr.next
+            pos += 1
+
+        if curr.next is None:
+            return
+
+        curr.next = curr.next.next
+        
+
+
+# Your MyLinkedList object will be instantiated and called as such:
+# obj = MyLinkedList()
+# param_1 = obj.get(index)
+# obj.addAtHead(val)
+# obj.addAtTail(val)
+# obj.addAtIndex(index,val)
+# obj.deleteAtIndex(index)
