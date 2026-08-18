8b0bda2c01c727798a2ae887b8edc658e23ca553
kalpeshkad29 <kalpeshjk1111@gmail.com>
2026-08-18 09:54:09 +0530

    Time: 23 ms (16.47%), Space: 19.3 MB (92.75%) - LeetHub

---
+++ 0496-next-greater-element-i/0496-next-greater-element-i.py
@@ -0,0 +1,18 @@
+class Solution:
+    def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        result = []
+
+        for num in nums1:
+            idx = nums2.index(num)
+
+            found = -1
+
+            for j in range(idx + 1, len(nums2)):
+                if nums2[j] > num:
+                    found = nums2[j]
+                    break
+
+            result.append(found)
+
+        return result
+        
