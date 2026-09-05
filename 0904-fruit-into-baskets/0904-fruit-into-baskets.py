78af24fa5af64f8f32effa868f068189744b7779
kalpeshkad29 <kalpeshjk1111@gmail.com>
2026-09-05 10:49:53 +0530

    Time: 159 ms (88.04%), Space: 26 MB (13.93%) - LeetHub

--- 0904-fruit-into-baskets/0904-fruit-into-baskets.py
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.py
@@ -7,7 +7,7 @@ class Solution:
         right=0
         while right<n:
             my_dict[fruits[right]]=my_dict.get(fruits[right],0)+1
-            while len(my_dict)>2:
+            if len(my_dict)>2:
                 my_dict[fruits[left]]-=1
                 if my_dict[fruits[left]]==0:
                     del my_dict[fruits[left]]
