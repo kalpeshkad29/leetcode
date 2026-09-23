f0c140c760d860a0ce9a00cf4713e7e3e8d4ba84
kalpeshkad29 <kalpeshjk1111@gmail.com>
2026-09-23 10:37:42 +0530

    Time: 672 ms (26.43%), Space: 0B (100%) - LeetHub

--- 3570-find-books-with-no-available-copies/3570-find-books-with-no-available-copies.sql
+++ 3570-find-books-with-no-available-copies/3570-find-books-with-no-available-copies.sql
@@ -6,5 +6,5 @@ from
 library_books l join borrowing_records b
 on l.book_id=b.book_id
 group by l.book_id,l.title,l.author,l.genre,l.publication_year,l.total_copies
-having current_borrowers=l.total_copies
+having sum(case when b.return_date is null then 1 else 0 end )=l.total_copies
 order by current_borrowers desc , l.title asc;
